0a309d9be39d9d7279b0d29b7a8bafa40bfebf03
esolzIos <esolzios16@gmail.com>
2017-06-10 12:53:22 +0530

    recent uploaded design Working on

---
+++ JokeMaster/JokeMaster/JMRecentUploadedCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  JMRecentUploadedCollectionViewCell.h
+//  JokeMaster
+//
+//  Created by priyanka on 10/06/17.
+//  Copyright © 2017 esolz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMRecentUploadedCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ JokeMaster/JokeMaster/JMRecentUploadedCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  JMRecentUploadedCollectionViewCell.m
+//  JokeMaster
+//
+//  Created by priyanka on 10/06/17.
+//  Copyright © 2017 esolz. All rights reserved.
+//
+
+#import "JMRecentUploadedCollectionViewCell.h"
+
+@implementation JMRecentUploadedCollectionViewCell
+
+@end
